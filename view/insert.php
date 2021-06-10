5220fddef4fac8385dcea27de14de721e2fa6fe4
david.propitech <david.propitech@gmail.com>
2021-06-10 16:30:51 +0300

    master

--- view/insert.php
+++ view/insert.php
@@ -7,20 +7,30 @@
             <input type="text" name="description_attr" id="description_attr">
             <button type="submit">Insert</button>
     </form>
+    <p>valeur css</p>
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <label for="select_attr">Attribut css </label>
+        <select name="attr_select" id="attr_select">
+            <?php
+                foreach($result as $c=>$v):
+                        echo "<option valeur=".$result[$c]->getValeur().">".$result[$c]->getValeur()."</option>";
+                endforeach;
+            ?>      
+        </select>
+        <label for="css">valeur</label>
+            <input type="text" name="valeur" id="valeur" required>
+        <label for="description">Description</label>
+            <input type="text" name="description_val" id="description_val">
+            <button type="submit">Insert</button>
+    </form>
 </div>
-<p>valeur css</p>
-<form action="<?php $_SERVER['PHP_SELF']?>" method="post">
-    <label for="select_attr">Attribut css </label>
-    <select name="attr_select" id="attr_select">
-        <?php
-            foreach($result as $c=>$v):
-                    echo "<option valeur=".$result[$c]->getValeur().">".$result[$c]->getValeur()."</option>";
-            endforeach;
-        ?>      
-    </select>
-    <label for="css">valeur</label>
-        <input type="text" name="valeur" id="valeur" required>
-    <label for="description">Description</label>
-        <input type="text" name="description_val" id="description_val">
-        <button type="submit">Insert</button>
-</form>
+<style>
+.insert{
+    position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  background-color: yellow;
+  padding: 5px;
+  font-size: 10px;
+}
+</style>
